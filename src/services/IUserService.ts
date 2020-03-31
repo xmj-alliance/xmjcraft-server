0b25ac89fa06caee800e8301b3b247327072ee06
valorad <valorad@outlook.com>
2020-03-31 18:11:24 -0400

    user service

--- src/services/IUserService.ts
+++ src/services/IUserService.ts
@@ -1,10 +1,13 @@
 import User from "../models/User";
 import CUDMessage from "../models/CUDMessage";
+import { ViewOption } from "../database/view";
 
 export interface IUserService {
-  getUser: (dbname: string) => User,
-  getUsers: (condition: any) => User[],
-  addUser: (user: User) => CUDMessage,
-  updateUser: (condition: any, token: any) => CUDMessage,
-  deleteUser: (dbname: string) => CUDMessage,
+  getUser: (dbname: string, options?: ViewOption) => Promise<User | null>,
+  getUsers: (condition: any, options?: ViewOption) => Promise<User[]>,
+  addUser: (user: User) => Promise<CUDMessage>,
+  updateUser: (dbname: string, token: any) => Promise<CUDMessage>,
+  updateUsers: (condition: any, token: any) => Promise<CUDMessage>,
+  deleteUser: (dbname: string) => Promise<CUDMessage>,
+  deleteUsers: (condition: any) => Promise<CUDMessage>,
 }
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,30 +1,129 @@
 import { IUserService } from "./IUserService";
 import User from "../models/User";
+import users from "../database/schema/userSchema"
+import { ViewOption, buildProjection, buildPagination, buildSort } from "../database/view";
 
 export default class UserService implements IUserService {
-  getUser: (dbname: string) => import("../models/User").default;
 
+  private col = users;
 
+  getUser = async (dbname: string, options: ViewOption = {}) => {
+    let projections = buildProjection(options);
+    return await this.col.findOne({dbname}, projections);
+  }
 
+  getUsers = async (condition: any, options: ViewOption = {}) => {
 
-  getUsers = (condition: any) => {
-    return [
-      {
-        dbname: "user-embro",
-        name: "Embro"
-      },
-      {
-        dbname: "user-dorian",
-        name: "Dorian"
-      },
-    ] as User[];
+    let projections = buildProjection(options);
+    let paginations = buildPagination(options);
+    let sort = buildSort(options);
+    return await this.col.find(condition, projections, paginations).sort(sort);
+  
   };
 
+  addUser = async (newUser: User) => {
+    try {
+      await this.col.create(newUser);
+    } catch (error) {
+      
+      return {
+        ok: false,
+        numAffected: 0,
+        message: error.message,
+      }
 
+    }
+    
+    return {
+      ok: true,
+      numAffected: 1,
+      message: "",
+    }
 
+  };
 
-  addUser: (user: import("../models/User").default) => import("../models/CUDMessage").default;
-  updateUser: (condition: any, token: any) => import("../models/CUDMessage").default;
-  deleteUser: (dbname: string) => import("../models/CUDMessage").default;
+  updateUser = async (dbname: string, token: any) => {
+
+    try {
+      let updateResult = await this.col.updateOne({dbname}, token);
+
+      return {
+        ok: true,
+        numAffected: updateResult.nModified,
+        message: "",
+      }
+
+    } catch (error) {
+      return {
+        ok: false,
+        numAffected: 0,
+        message: error.message,
+      }
+    }
+
+  };
+
+  updateUsers = async (condition: any, token: any) => {
+
+    try {
+      let updateResult = await this.col.update(condition, token, {multi: true});
+
+      return {
+        ok: true,
+        numAffected: updateResult.nModified,
+        message: "",
+      }
+
+    } catch (error) {
+      return {
+        ok: false,
+        numAffected: 0,
+        message: error.message,
+      }
+    }
+
+  };
   
+  deleteUser = async (dbname: string) => {
+
+    try {
+      let deleteResult = await this.col.deleteOne({dbname});
+
+      return {
+        ok: true,
+        numAffected: deleteResult.deletedCount || 0,
+        message: "",
+      }
+
+    } catch (error) {
+      return {
+        ok: false,
+        numAffected: 0,
+        message: error.message,
+      }
+    }
+
+  };
+
+  deleteUsers = async (condition: any) => {
+
+    try {
+      let deleteResult = await this.col.remove(condition);
+
+      return {
+        ok: true,
+        numAffected: deleteResult.deletedCount || 0,
+        message: "",
+      }
+
+    } catch (error) {
+      return {
+        ok: false,
+        numAffected: 0,
+        message: error.message,
+      }
+    }
+
+  };
+
 }
