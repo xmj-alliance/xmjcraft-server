7395b3edbd1ee73bee9a946d897247ed3320c611
valorad <valorad@outlook.com>
2020-03-31 18:11:01 -0400

    user schema

---
+++ src/database/schema/userSchema.ts
@@ -0,0 +1,19 @@
+import { model, Schema, Document } from "mongoose";
+import User from "../../models/User";
+
+interface UserDoc extends User, Document {}
+
+const schema = new Schema({
+  dbname: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {
+    type: String,
+    required: false
+  },
+
+});
+
+export default model<UserDoc>("users", schema, "jinmoes");
